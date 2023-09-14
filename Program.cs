0a606f9e8d60e459251a1d073eb6b9587617e649
Hibblr1 <hibblr1@student.op.ac.nz>
2023-09-14 15:55:21 +1200

    help2

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestDoc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("sugma"); //edited by Rogan Hibbu
+        }
+    }
+}
